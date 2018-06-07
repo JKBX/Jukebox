1807ed0e996ff7709cd3c24d2014e817679e36cd
Philipp Altmann <philippaltmann@deap.io>
2018-06-07 15:29:32 +0200

    Added Class for parsing tracks

---
+++ Jukebox/PartyPlaylist/TrackModel.swift
@@ -0,0 +1,30 @@
+//
+//  TrackModel.swift
+//  Jukebox
+//
+//  Created by Philipp on 06.06.18.
+//  Copyright © 2018 Jukebox. All rights reserved.
+//
+import FirebaseDatabase
+import FirebaseAuth
+class Track{
+    var id: String!
+    var name: String!
+    var artist: String!
+    var album: String!
+    var coverUrl: URL!
+    var voteCount: UInt!
+    var liked: Bool
+    
+    init(from snapshot: DataSnapshot){
+        let userId = Auth.auth().currentUser?.uid as! String
+        
+        self.id = snapshot.key
+        self.name = snapshot.childSnapshot(forPath: "songTitle").value as! String
+        self.artist = snapshot.childSnapshot(forPath: "artist").value as! String
+        self.album = snapshot.childSnapshot(forPath: "albumTitle").value as! String
+        self.coverUrl = URL(string: snapshot.childSnapshot(forPath: "coverURL").value as! String)!
+        self.liked = snapshot.childSnapshot(forPath: "votes/\(userId)").exists()
+        self.voteCount = snapshot.childSnapshot(forPath: "votes").childrenCount
+    }
+}
