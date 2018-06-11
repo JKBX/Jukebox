ac685653441194f37a6c4ab72360d4902bcfce19
Anja Schweiger <Anja.Schweiger@campus.lmu.de>
2018-06-11 14:30:16 +0200

    added TextField PartyID + limited characters

--- Jukebox/PartyOverview/JoinPartyViewController.swift
+++ Jukebox/PartyOverview/JoinPartyViewController.swift
@@ -8,31 +8,29 @@
 
 import UIKit
 
+
 class JoinPartyViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet var digitOne: UITextField!
-    @IBOutlet var digitTwo: UITextField!
-    @IBOutlet var digitThree: UITextField!
-    @IBOutlet var digitFour: UITextField!
-    @IBOutlet var digitFive: UITextField!
-    @IBOutlet var digitSix: UITextField!
+ 
+    @IBOutlet var IDTextField: UITextField!
     
+    @IBOutlet weak var userMobileNoTextFiled: UITextField!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        digitOne.delegate = self
-
-        // Do any additional setup after loading the view.
-    }
-    func digitOne(digitOne: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         
-        let maxLength = 1
-        let currentString: NSString = digitOne.text! as NSString
-        let newString: NSString =
-            currentString.replacingCharacters(in: range, with: string) as NSString
-        return newString.length <= maxLength
-    }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            IDTextField.delegate = self                  //set delegate
+        }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+                       replacementString string: String) -> Bool
+        {
+            let maxLength = 6
+            let currentString: NSString = textField.text! as NSString
+            let newString: NSString =
+                currentString.replacingCharacters(in: range, with: string) as NSString
+            return newString.length <= maxLength
+        }
 
     @IBAction func done(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
