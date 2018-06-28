0576fd2fb3ccec219c6143adf54aa3466da3ed79
Philipp Altmann <philippaltmann@deap.io>
2018-06-29 01:22:06 +0200

    Updated Edit Icon & Replaced KF image processor

--- Jukebox/Auth/AuthViewController.swift
+++ Jukebox/Auth/AuthViewController.swift
@@ -2,94 +2,34 @@
 //  AuthViewController.swift
 //  Jukebox
 //
-//  Created by Philipp on 20.06.18.
+//  Created by Philipp on 28.06.18.
 //  Copyright © 2018 Jukebox. All rights reserved.
 //
 
 import UIKit
-import FirebaseAuth
 
 class AuthViewController: UIViewController {
 
-    @IBOutlet weak var LoginView: UIView!
-    @IBOutlet weak var ProfileView: UIView!
-    @IBOutlet weak var done: UIBarButtonItem!
-    
-    //Loading View
-    var blurredBackgroundView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        ProfileView.isHidden = true
-        let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
-        
-        
-        Auth.auth().addIDTokenDidChangeListener { (auth, _) in
-            print(auth.currentUser)
-            if auth.currentUser != nil {
-                self.done.isEnabled = auth.currentUser?.displayName != nil
-                self.switchView(to: "profileView")
-            } else {
-                print(auth.currentUser?.displayName == nil)
-                self.done.isEnabled = false
-                self.switchView(to: "loginView")
-            }
-            
-        }
-        //NotificationCenter.default.addObserver(self, selector: #selector(successfulLogin), name: NSNotification.Name.Spotify.loggedIn, object: nil)
-        
-        
-        self.blurredBackgroundView.frame = view.bounds
-        
-        NotificationCenter.default.addObserver(forName: NSNotification.Name.Auth.loading, object: nil, queue: nil) { (e) in
-            let notification = e.object as! (loading:Bool, message:String)
-            
-            if notification.loading {
-                self.view.addSubview(self.blurredBackgroundView)
-                self.present(self.getAlert(for: notification.message), animated: true, completion: nil)
-            } else {
-                self.dismiss(animated: true, completion: nil)
-                self.blurredBackgroundView.removeFromSuperview()
-            }  
-        }
 
         // Do any additional setup after loading the view.
     }
-    
-    func getAlert(for message: String) -> UIAlertController {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
-        loadingIndicator.hidesWhenStopped = true
-        loadingIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
-        loadingIndicator.startAnimating();
-        alert.view.addSubview(loadingIndicator)
-        return alert
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    func switchView(to: String) {
-        //TODO switch title
-        let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
-        
-        if to == "profileView"{
-            UIView.transition(with: LoginView, duration: 0.8, options: transitionOptions, animations: {
-                self.LoginView.isHidden = true
-            })
-            UIView.transition(with: ProfileView, duration: 0.8, options: transitionOptions, animations: {
-                self.ProfileView.isHidden = false
-            })
-        } else {
-            UIView.transition(with: ProfileView, duration: 0.8, options: transitionOptions, animations: {
-                self.ProfileView.isHidden = true
-            })
-            UIView.transition(with: LoginView, duration: 0.8, options: transitionOptions, animations: {
-                self.LoginView.isHidden = false
-            })
-        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
-    @IBAction func done(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
+    */
+
 }
---
+++ Jukebox/Auth/CenteredSquareProcessor.swift
@@ -0,0 +1,9 @@
+//
+//  CenteredSquareProcessor.swift
+//  Jukebox
+//
+//  Created by Philipp on 28.06.18.
+//  Copyright © 2018 Jukebox. All rights reserved.
+//
+
+import Foundation
