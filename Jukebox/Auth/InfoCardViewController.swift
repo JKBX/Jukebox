a75504642b710af6251718be629a27386d9e4017
Philipp Altmann <philippaltmann@deap.io>
2018-06-29 18:37:58 +0200

    Finalized Info Card

--- Jukebox/Auth/InfoCardViewController.swift
+++ Jukebox/Auth/InfoCardViewController.swift
@@ -13,46 +13,21 @@ import Kingfisher
 class InfoCardViewController: UIViewController {
     
     var delegate: CardDelegate?
-
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var username: UILabel!
     @IBOutlet weak var picture: UIImageView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-        Auth.auth().addIDTokenDidChangeListener { (auth, _) in
-            if auth.currentUser != nil {
-                self.name.text = auth.currentUser?.displayName
-                self.username.text = auth.currentUser?.uid
-                self.picture.kf.setImage(with: Auth.auth().currentUser?.photoURL, options: [.processor(CenteredSquareProcessor())])
-                self.picture.layer.masksToBounds = true
-                self.picture.layer.cornerRadius = 64.0
-                
-                //self.picture.kf.setImage(with: auth.currentUser?.photoURL)
-            }
-        }
-        // Do any additional setup after loading the view.
+    
+    func update(_ user: User) {
+        self.name.text = user.displayName
+        self.username.text = user.uid
+        if let image = user.photoURL {
+            self.picture.kf.setImage(with: image)
+        } else { self.picture.image = UIImage(named: "ProfilePictureDefault") }
+        self.picture.layer.masksToBounds = true
+        self.picture.layer.cornerRadius = 64.0
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        
+    @IBAction func edit(_ sender: Any){
+        self.delegate?.editing(true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
