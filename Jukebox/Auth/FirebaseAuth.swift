019ce1a5b18b9d36843f613979e97e52180f3867
Philipp Altmann <philippaltmann@deap.io>
2018-06-29 18:33:09 +0200

    Extended Firebase Auth to enable login with spotify credentials

--- Jukebox/Auth/FirebaseAuth.swift
+++ Jukebox/Auth/FirebaseAuth.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2018 Jukebox. All rights reserved.
 //
 
-import Foundation
+import FirebaseAuth
+import Alamofire
+
+extension Auth {
+    func signIn(with session: SPTSession?, completion: @escaping (User?, Error?) -> Void) {
+        guard let session = session else { completion(nil, NSError(domain: "Custom Auth", code: 401, userInfo: [NSLocalizedDescriptionKey : "No Session found"])); return}
+        let headers: HTTPHeaders = [ "Authorization": "Bearer \(session.accessToken!)" ]
+        Alamofire.request(URL(string: "https://jkbx-swapify.herokuapp.com/authenticate")!, method: .get, headers: headers).responseString { (response) in
+            if let error = response.error { completion(nil, error); return }
+            let JWToken = response.value!
+            self.signIn(withCustomToken: JWToken, completion: { (response, error) in
+                if let error = error { completion(nil, error); return }
+                if let response = response {
+                    completion(response.user, nil)
+                } else {completion(nil, NSError(domain: "Custom Auth", code: 404, userInfo: [NSLocalizedDescriptionKey : "An error occured"]));}
+                return
+            })
+        }
+    }
+}
