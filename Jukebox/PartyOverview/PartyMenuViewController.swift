9b5cb738e560a93c380fbb676949a1a6244be347
Maximilian Babel <maximilian.babel@campus.lmu.de>
2018-07-01 16:11:28 +0200

    added functions for partymenu UI

--- Jukebox/PartyOverview/PartyMenuViewController.swift
+++ Jukebox/PartyOverview/PartyMenuViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Firebase
+import Firebase
 
 class PartyMenuViewController: UIViewController {
+    
+    var ref: DatabaseReference!
+    var user = Auth.auth().currentUser
+    var shareMessage: String = "This is your PartyID: \n \(currentParty)"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.ref = Database.database().reference()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +28,33 @@ class PartyMenuViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func dismissPopUp(_ sender: UIButton) {
+    @IBAction func sharePartyIDPressed(_ sender: ButtonDesignable) {
+        let activity = UIActivityViewController(activityItems: [shareMessage], applicationActivities: nil)
+        activity.popoverPresentationController?.sourceView = self.view
+        self.present(activity, animated: true, completion: nil)
+    }
+    
+    @IBAction func deletePartyPressed(_ sender: ButtonDesignable) {
+        let alert = UIAlertController(title: "Delete", message: "Do you want to delete the party \(currentParty)", preferredStyle: .alert)
+        let cancleAction = UIAlertAction(title: "Cancle", style: .cancel, handler: nil)
+        let deleteParty = UIAlertAction(title: "Delete", style: .destructive) { (action) in
+            self.extinguishPartyFromFirebase()
+        }
+        
+        alert.addAction(cancleAction)
+        alert.addAction(deleteParty)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func extinguishPartyFromFirebase() {
+        if(currentAdmin == false){
+            self.ref.child("/users/\(user?.uid)/parties/\(currentParty)").setValue(nil)
+        } else {
+            print("Du bist Admin dieser Party!")
+        }
+    }
+    
+    @IBAction func dismissPopUp(_ sender: ButtonDesignable) {
         dismiss(animated: true, completion: nil)
     }
 }
