f2d4158152df3c8678b1071716da29849b39307f
Philipp Altmann <philippaltmann@deap.io>
2018-07-02 16:06:56 +0200

    Implemented audio broadcasting for party guests

--- Jukebox/AudioStreamingDelegate.swift
+++ Jukebox/AudioStreamingDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AudioStreamingDelegate.swift
-//  test
+//  Jukebox
 //
 //  Created by Philipp on 28.04.18.
 //  Copyright © 2018 Philipp. All rights reserved.
@@ -13,15 +13,15 @@ import Kingfisher
 
 class AudioStreamingDelegate: NSObject {
 
-    //TODO
-    //TODO move to streaming delegate
-    //SPTAudioStreamingController.sharedInstance().login(withAccessToken: session.accessToken)
-
     var partyTMP: String?
+    var updatingBroadcast = false
 
     func willUpdate() {
         partyTMP = currentParty
     }
+    
+    typealias PlaybackStatus = (position: TimeInterval, time: TimeInterval, delay: TimeInterval)
+    
      
 
     func update() {
@@ -37,17 +37,18 @@ class AudioStreamingDelegate: NSObject {
     }
 
     @objc func prev() {
+        //TODO write correct pos to firebase
         let ref = Database.database().reference().child("/parties/\(currentParty)/currentlyPlaying/playbackStatus")
         SPTAudioStreamingController.sharedInstance().seek(to: 0) { (error) in
             if let error = error {print(error); return}
-            ref.setValue(["position": currentTrackPosition, "time": NSDate.timeIntervalSinceReferenceDate])
+            ref.setValue(["position": SPTAudioStreamingController.sharedInstance().playbackState.position, "time": NSDate.timeIntervalSinceReferenceDate])
         }
     }
 
     @objc func next() {
         currentTrackPosition = 0.0
         if (currentTrack?.isPlaying)!{
-            getNextTrack{ self.play() }
+            self.getNextTrack{  self.play() }
         }
         else{
             getNextTrack{ self.pause(){}}
@@ -58,16 +59,14 @@ class AudioStreamingDelegate: NSObject {
         print("Toggle")
         if currentTrack == nil{
             print("curr is nil")
-            getNextTrack{ self.play() }
+            self.getNextTrack{ self.play() }
         } else {
             if (currentTrack?.isPlaying)! { pause(){} }
-            else { play() }
+            else { self.play() }
         }
     }
 
     func play() {
-        print("Play")
-        print(currentAdmin)
         if(currentAdmin){
             let ref = Database.database().reference().child("/parties/\(currentParty)")
             if let currentMetadata = SPTAudioStreamingController.sharedInstance().metadata{
@@ -76,19 +75,15 @@ class AudioStreamingDelegate: NSObject {
                 if id == currentTrack?.trackId{
                     SPTAudioStreamingController.sharedInstance().setIsPlaying(true) { (error) in
                         if let error = error { print(error); return}
-                        ref.child("currentlyPlaying/isPlaying").setValue(true)
-                        ref.child("currentlyPlaying/playbackStatus").setValue(["position": currentTrackPosition, "time": NSDate.timeIntervalSinceReferenceDate])
-                        return
                     }
+                    return
                 }
             }
             let trackId = currentTrack?.trackId
             SPTAudioStreamingController.sharedInstance().playSpotifyURI("spotify:track:\(trackId!)", startingWith: 0, startingWithPosition: (currentTrack?.playbackStatus?.position)!, callback: { (error) in
                 if let error = error { print(error); return }
-                ref.child("currentlyPlaying/isPlaying").setValue(true)
-                ref.child("currentlyPlaying/playbackStatus").setValue(["position":currentTrackPosition, "time": NSDate.timeIntervalSinceReferenceDate])
             })
-  
+            
         }
         
     }
@@ -206,13 +201,12 @@ extension AudioStreamingDelegate {
             self.prev(); return .success }
         let startHandler: (MPRemoteCommandEvent) -> MPRemoteCommandHandlerStatus = {
             (event) -> MPRemoteCommandHandlerStatus in
-            self.start(); return .success }
+            self.updateBroadcast(); return .success }
         let stopHandler: (MPRemoteCommandEvent) -> MPRemoteCommandHandlerStatus = {
             (event) -> MPRemoteCommandHandlerStatus in
             self.stop(){}; return .success }
 
         if currentAdmin {
-            print("Setting Admin commands")
             commandCenter.playCommand.addTarget(handler: playHandler)
             commandCenter.pauseCommand.addTarget(handler: pauseHandler)
             commandCenter.nextTrackCommand.addTarget(handler: nextHandler)
@@ -245,54 +239,111 @@ extension AudioStreamingDelegate: SPTAudioStreamingDelegate{
 
     func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
         print("Did Login")
-
         NotificationCenter.default.post(name: NSNotification.Name.Spotify.loggedIn, object: nil)
         SPTAudioStreamingController.sharedInstance().playbackDelegate = self
         NotificationCenter.default.addObserver(self, selector: #selector(toggle), name: NSNotification.Name.Spotify.toggle, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(next), name: NSNotification.Name.Spotify.nextSong, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(prev), name: NSNotification.Name.Spotify.prevSong, object: nil)
-
+        NotificationCenter.default.addObserver(self, selector: #selector(broadcast), name: NSNotification.Name.Spotify.broadcast, object: nil)
+        //NotificationCenter.default.addObserver(self, selector: #selector(stop), name: NSNotification.Name.Spotify.stopBroadcast, object: nil)
+    
 //        OBSERVER FÜR Broadcast
         
         setupAudioSession()
         do { try AVAudioSession.sharedInstance().setActive(true) }
         catch let error as NSError { print(error.localizedDescription) }
     }
-  func start() {
-        print("Starting Broadcast")
-        isBroadcasting = true
+    @objc func broadcast(){
+        if isBroadcasting {
+            
+            NotificationCenter.default.removeObserver(self, name: NSNotification.Name.Player.trackChanged, object: nil)
+            DispatchQueue.main.async(execute: {() -> Void in
+            self.stop {
+                print("Stopped \(Thread.current)")
+                isBroadcasting = false
+                NotificationCenter.default.post(name: NSNotification.Name.Player.broadcast, object: nil)
+            }
+            })
+        } else {
+            NotificationCenter.default.addObserver(self, selector: #selector(updateBroadcast), name: NSNotification.Name.Player.trackChanged, object: nil)
+            updateBroadcast()
+        }
+    }
+    //TODO 3 states for broadcast: inactive active updating
+    //TODO update status from host
+    //TODO deactivate broadcast when failing
+    @objc func updateBroadcast(){
+        if self.updatingBroadcast{print("Already Updating"); return}
+        self.updatingBroadcast = true
+        if let currentTrack = currentTrack{
+            if let status = currentTrack.playbackStatus as? PlaybackStatus{
+                if currentTrack.isPlaying{
+                    DispatchQueue.main.async(execute: {() -> Void in
+                        self.stop {
+                            self.start(currentTrack, status: status, completion: { (success, trial) in
+                                print("\(success ? "": "Not ")Playing after \(trial) trials")
+                                self.updatingBroadcast = false
+                                isBroadcasting = success
+                                NotificationCenter.default.post(name: NSNotification.Name.Player.broadcast, object: nil)
+                            })
+                        }
+                    })
+                } else {print("Not Playing or delay not set"); stop{self.updatingBroadcast = false}}
+            } else {print("No Playback status"); stop{self.updatingBroadcast = false}}
+        } else {print("No Current Track"); stop{self.updatingBroadcast = false}}
     }
     
-    func stop(completion: ()->Void) {
-        print("Stopping Broadcast")
+    func start(_ currentTrack: TrackModel, status: PlaybackStatus, trial: Int? = 0,completion: @escaping (_: Bool, _: Int)->Void) {
+        let maxRepetitions = 10
+        let trial = trial ?? 0
+        if trial > maxRepetitions {self.stop { completion(false, trial) }; return}
         
-        isBroadcasting = false
-        completion()
+        
+        let id = currentTrack.trackId
+        let delay = NSDate.timeIntervalSinceReferenceDate - status.time - status.delay
+        let position = status.position + delay
+        let extendedPosition = position + 1
+        
+        let track = "spotify:track:\(id!)"
+        SPTAudioStreamingController.sharedInstance().playSpotifyURI(track, startingWith: 0, startingWithPosition: extendedPosition, callback: { (error) in
+            if let error = error { print(error); self.stop{completion(false, trial)}; return }
+            
+            guard let metadata = SPTAudioStreamingController.sharedInstance().metadata else {
+                self.stop { self.start(currentTrack, status: status, trial: trial+1, completion: { (success, trial) in completion(success, trial) })}; return
+            }
+            if let currentPlayback = metadata.currentTrack {
+                if currentPlayback.uri != track && trial == 0 {
+                    sleep(1); self.stop { self.start(currentTrack, status: status, trial: trial+1, completion: { (success, trial) in completion(success, trial) })}; return
+                }
+            } else {
+                if trial == 0{
+                    sleep(1); self.stop { self.start(currentTrack, status: status, trial: trial+1, completion: { (success, trial) in completion(success, trial) })}; return }
+            }
+            
+            repeat {
+                usleep(1000)
+            } while (self.getDelay(from: status) < 0)
+            print(self.getDelay(from: status))
+            
+            if abs(self.getDelay(from: status)) > 0.01 {
+                self.stop { self.start(currentTrack, status: status, trial: trial+1, completion: { (success, trial) in completion(success, trial) })}; return
+            }
+            completion(true, trial)
+            return
+        })
+        
     }
     
-//    @objc func startBroad(){
-//
-////        print("BROADCASTING START")
-//////        let ref = Database.database().reference().child("/parties/\(currentParty)/currentlyPlaying/")
-////                let trackId = currentTrack?.trackId
-////            SPTAudioStreamingController.sharedInstance().playSpotifyURI("spotify:track:\(trackId!)", startingWith: 0, startingWithPosition: 0, callback: { (error) in
-////                if let error = error { print(error); return }
-////            })
-////
-//////        ref.child("isPosition").observe(.value) { (snapshot) in
-//////            currentTrackPosition = snapshot.value as! TimeInterval
-////////            currentTrackPosition = snapshot.hasChild("isPosition") ? snapshot.childSnapshot(forPath: "isPosition").value as! TimeInterval : 0.0
-//////        }
-//////
-//////      print(currentTrackPosition, "Track Position")
-////
-// }
+    func getDelay(from status: (position: TimeInterval, time: TimeInterval, delay: TimeInterval)) -> TimeInterval {
+        return status.position + NSDate.timeIntervalSinceReferenceDate - status.time + status.delay-SPTAudioStreamingController.sharedInstance().playbackState.position
+    }
     
-//    @objc func stopBroad(){
-//        SPTAudioStreamingController.sharedInstance().setIsPlaying(false) { (error) in
-//            if let error = error { print(error); return }}
-//
-//    }
+    func stop(completion: @escaping ()->Void) {
+        SPTAudioStreamingController.sharedInstance().setIsPlaying(false) { (error) in
+            if let error = error {print(error); completion();return}
+            completion()
+        }
+    }
 
     func audioStreaming(_ audioStreaming: SPTAudioStreamingController!, didReceiveError error: Error!) {
         print("Did receive Error")
@@ -304,11 +355,13 @@ extension AudioStreamingDelegate: SPTAudioStreamingDelegate{
     }
 
     func audioStreamingDidLogout(_ audioStreaming: SPTAudioStreamingController!) {
-        print("Did Logout")
-
         do { try AVAudioSession.sharedInstance().setActive(false) }
         catch let error as NSError { print(error.localizedDescription) }
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.Spotify.toggle, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.Spotify.nextSong, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.Spotify.prevSong, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.Spotify.broadcast, object: nil)
+        
     }
 
     func audioStreamingDidEncounterTemporaryConnectionError(_ audioStreaming: SPTAudioStreamingController!) {
@@ -332,6 +385,14 @@ extension AudioStreamingDelegate: SPTAudioStreamingPlaybackDelegate{
 
     func audioStreaming(_ audioStreaming: SPTAudioStreamingController!, didChangePlaybackStatus isPlaying: Bool) {
         updatePlayingCenter(audioStreaming)
+        let ref = Database.database().reference().child("/parties/\(currentParty)/currentlyPlaying")
+        if isPlaying && currentAdmin {
+            ref.child("isPlaying").setValue(true)
+            let position = SPTAudioStreamingController.sharedInstance().playbackState.position
+            let time = NSDate.timeIntervalSinceReferenceDate
+            
+            ref.child("playbackStatus").setValue(["position":position, "time": time, "delay": nil])
+        }
     }
 
 
@@ -344,15 +405,44 @@ extension AudioStreamingDelegate: SPTAudioStreamingPlaybackDelegate{
     }
 
     func audioStreaming(_ audioStreaming: SPTAudioStreamingController!, didStopPlayingTrack trackUri: String!) {
-        if (currentTrack?.isPlaying)! {
+        if (currentTrack?.isPlaying)! && currentAdmin{
             print("Track stopped, need to get new one")
-            self.getNextTrack { self.play() }
+            /*DispatchQueue.main.async(execute: {() -> Void in*/ self.getNextTrack { self.play() } //})
         }
     }
 
     func audioStreaming(_ audioStreaming: SPTAudioStreamingController!, didChangePosition position: TimeInterval) {
         currentTrackPosition = position
-        NotificationCenter.default.post(name: NSNotification.Name.Player.position, object: nil)        
+        NotificationCenter.default.post(name: NSNotification.Name.Player.position, object: nil)
+        if let currentTrack = currentTrack {
+            if currentTrack.isPlaying{
+               if let status = currentTrack.playbackStatus as? (position: TimeInterval, time: TimeInterval, delay: TimeInterval?) {
+                
+                guard let statusDelay = status.delay else {
+                    let ref = Database.database().reference().child("/parties/\(currentParty)/currentlyPlaying")
+                    let should = status.position + NSDate.timeIntervalSinceReferenceDate - status.time
+                    let delay = should-SPTAudioStreamingController.sharedInstance().playbackState.position
+                    ref.child("playbackStatus/delay").setValue(delay)
+                    return
+                }
+                
+                
+                let should = status.position + NSDate.timeIntervalSinceReferenceDate - status.time
+                let delay = should-position//SPTAudioStreamingController.sharedInstance().playbackState.position
+                let misplacedBy = delay-statusDelay
+                if delay > 0.1 {
+                    print("Delay is: \(delay)\nShould be: \(statusDelay)")
+                    self.updateBroadcast()
+                }
+                
+                //let should = status.position + NSDate.timeIntervalSinceReferenceDate - status.time //- status.delay
+                  //  print(should-position+delay)
+                //print(status.delay)
+                
+                //let should = status.position + NSDate.timeIntervalSinceReferenceDate - status.time - position
+               } else { print("Status not set ") }
+            }
+        }
     }
     
 
@@ -363,9 +453,11 @@ extension AudioStreamingDelegate: SPTAudioStreamingPlaybackDelegate{
     }
 
     func audioStreamingDidBecomeInactivePlaybackDevice(_ audioStreaming: SPTAudioStreamingController!) {
+        print("Did become inactive")
     }
 
-
+    //TODO Did receive Error
+    //some(Error Domain=com.spotify.ios-sdk.playback Code=1006 "Context failed"
 
     // Mark - Unsupported Functions
   
