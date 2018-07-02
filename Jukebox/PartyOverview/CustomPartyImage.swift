95c372b1ea8c5e0e217f336c1483b85a16db07c4
Anja Schweiger <Anja.Schweiger@campus.lmu.de>
2018-07-02 14:54:33 +0200

    started image transformation

--- Jukebox/PartyOverview/CustomPartyImage.swift
+++ Jukebox/PartyOverview/CustomPartyImage.swift
@@ -7,14 +7,26 @@
 //
 
 import Foundation
+import UIKit
 
 class customPartyImage: UIImageView {
     override func setNeedsLayout() {
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: self.frame.width, y: self.frame.height))
+        path.move(to: .zero)
         path.addLine(to: CGPoint(x: self.frame.width/2, y: self.frame.height/2))
         path.addLine(to: CGPoint(x: self.frame.width/2, y: self.frame.height))
         path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height/2))
-
+        path.addLine(to: .zero)
+        
+        path.close()
+        UIColor.red.setFill()
+        path.stroke()
+        path.reversing()
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.frame = self.bounds
+        shapeLayer.path = path.cgPath
+        shapeLayer.fillColor = UIColor.red.cgColor
+        self.layer.mask = shapeLayer;
+        self.layer.masksToBounds = true;
     }
 }
