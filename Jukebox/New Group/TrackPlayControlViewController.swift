5617d16afb7e31489654a77057d06306f7428bcc
Philipp Altmann <philippaltmann@deap.io>
2018-07-03 08:08:16 +0200

    Adapted to 3 state broadcasting

--- Jukebox/New Group/TrackPlayControlViewController.swift
+++ Jukebox/New Group/TrackPlayControlViewController.swift
@@ -14,10 +14,12 @@ class TrackPlayControlViewController: UIViewController {
     
     @IBOutlet weak var songTitle: MarqueeLabel!
     @IBOutlet weak var artist: MarqueeLabel!
-    @IBOutlet weak var songDuration: UILabel!
+    @IBOutlet weak var elapsed: UILabel!
+    @IBOutlet weak var duration: UILabel!
     @IBOutlet weak var previousButton: UIButton!
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var position: UISlider!
     var timer: Timer!
     var nextSwitch:Bool = true
     var triggerPlayNext:Bool = true
@@ -30,27 +32,20 @@ class TrackPlayControlViewController: UIViewController {
         marqueeLabelTrackPlayer(MarqueeLabel: artist)
         setFields()
         updateDuration()
+        playPause()
     
         NotificationCenter.default.addObserver(forName: NSNotification.Name.Player.trackChanged, object: nil, queue: nil) { (note) in
             self.setFields()
-            self.nextButtonTrigger()
+            if self.nextSwitch {self.playPause()}
+            self.updateDuration() // TODO skip back on paused 
         }
-        NotificationCenter.default.addObserver(forName: NSNotification.Name.Player.position, object: nil, queue: nil) { (note) in
-            self.updateDuration()
-        }
-        NotificationCenter.default.addObserver(forName: NSNotification.Name.Player.isPlay, object: nil, queue: nil) { (note) in
-            self.playPause()
-        }
-        timer = Timer.init()
+        setupPositionSlider()
  }
     
     override func viewWillAppear(_ animated: Bool) {
         playPauseButton.isHidden = !currentAdmin
         previousButton.isHidden = !currentAdmin
         nextButton.isHidden = !currentAdmin
-        nextButtonTrigger()
-
-        setPlayPause()
    }
     
     @IBAction func playButton(_ sender: Any) {
@@ -61,7 +56,6 @@ class TrackPlayControlViewController: UIViewController {
     
     @IBAction func previousButton(_ sender: Any) {
         AudioServicesPlaySystemSound(1519)
-        songDuration.text = "00:00"
         nextSwitch = false
         NotificationCenter.default.post(name: NSNotification.Name.Spotify.prevSong, object: nil)
     }
@@ -73,7 +67,8 @@ class TrackPlayControlViewController: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5){
             NotificationCenter.default.post(name: NSNotification.Name.Spotify.nextSong, object: nil)
             self.nextButton.isEnabled = true
-            self.songDuration.text = "00:00"
+            self.elapsed.text = "00:00"
+            self.duration.text = ""
         }
     }
 }
@@ -93,64 +88,55 @@ extension TrackPlayControlViewController{
   }
 
     func playPause () {
-        if(currentTrack == nil){return}
-        if(nextSwitch)
-        {UIView.animate(withDuration: 0.3, animations: {
-            self.playPauseButton.alpha = 0.3
-        }, completion: {(finished) in
-            if(currentTrack?.isPlaying)!{
-                self.playPauseButton.setImage(UIImage(named: "baseline_pause_circle_outline_white_36pt"), for: .normal)
+        if let currentTrack = currentTrack{
+            if currentTrack.isPlaying{
+                self.playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+                self.playPauseButton.setImage(UIImage(named: "pausePressed"), for: .highlighted)
             }else{
-                self.playPauseButton.setImage(UIImage(named: "baseline_play_circle_outline_white_36pt"), for: .normal)
+                self.playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+                self.playPauseButton.setImage(UIImage(named: "playPressed"), for: .highlighted)
             }
-            UIView.animate(withDuration: 0.2, animations:{
-                self.playPauseButton.alpha = 1.0
-            },completion:nil)
-        })}
-        
-    }
-    
-    func setPlayPause(){
-        if((currentTrack?.isPlaying)!){
-            playPauseButton.setImage(UIImage(named: "baseline_pause_circle_outline_white_36pt"), for: .normal)
-        }else{playPauseButton.setImage(UIImage(named: "baseline_play_circle_outline_white_36pt"), for: .normal)}
+        }
     }
 
     func updateDuration() {
-        if(currentAdmin){
-            
-            var durationTime: String{
-                let formatter = DateFormatter()
-                formatter.dateFormat = "mm:ss"
-                let date = Date(timeIntervalSince1970: currentTrackPosition)
-                return formatter.string(from: date)
-            }
-            
-            self.songDuration.text = durationTime
-        }
-        else{
-            if(currentTrack == nil){return}
-            if (currentTrack?.isPlaying)!{
-                
+        if let currentTrack = currentTrack {
+            if currentTrack.isPlaying {
                 resetTimer()
-                
-                var position: TimeInterval = (currentTrack?.playbackStatus?.position)!
-                
-                timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
-                
-                var durationTime: String{
-                    let formatter = DateFormatter()
-                    formatter.dateFormat = "mm:ss"
-                    let date = Date(timeIntervalSince1970: position)
-                    return formatter.string(from: date)
-                }
-                
-                self.songDuration.text = durationTime
-                
-                            })
-                        } else {
-                            resetTimer()}
-        } }
+                guard let status = currentTrack.playbackStatus else { return }
+                let duration = floor(TimeInterval(currentTrack.duration) / 1000)
+                var elapsed = (status.position + NSDate.timeIntervalSinceReferenceDate - status.time)
+                timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (timer) in
+                    let position = self.getDurationData(duration: duration, elapsed: elapsed)
+                    self.elapsed.text = position.elapsed
+                    self.duration.text = position.duration
+                    self.position.setValue(position.position, animated: true)
+                    elapsed += 0.1
+                })
+            } else {
+                print("Else")
+                resetTimer()
+                guard let status = currentTrack.playbackStatus else { return }
+                let duration = floor(TimeInterval(currentTrack.duration) / 1000)
+                let elapsed = (status.position + NSDate.timeIntervalSinceReferenceDate - status.time)
+                let position = self.getDurationData(duration: duration, elapsed: elapsed)
+                self.elapsed.text = position.elapsed
+                self.duration.text = position.duration
+                self.position.setValue(position.position, animated: true)
+            }
+        }
+    }
+    
+    func getDurationData(duration: TimeInterval, elapsed: TimeInterval)
+        -> (elapsed: String, duration: String, position: Float) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "mm:ss"
+        let elapsedDate = Date(timeIntervalSince1970: floor(elapsed))
+        let durationDate = Date(timeIntervalSince1970: duration - floor(elapsed))
+        return (elapsed: formatter.string(from: elapsedDate),
+                duration: "-\(formatter.string(from: durationDate))",
+                position: Float(elapsed/duration) )
+    }
     
     func resetTimer() {
         if timer != nil{
@@ -159,23 +145,17 @@ extension TrackPlayControlViewController{
         }
     }
     
-    func nextButtonTrigger(){
-        if(currentQueue.count >= 1){self.nextButton.isEnabled = true}else{self.nextButton.isEnabled = false}
-    }
     
-        
+    func setupPositionSlider(){
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 8, height: 8), false, 0)
+        let ctx = UIGraphicsGetCurrentContext()!
+        ctx.saveGState()
+        let rect = CGRect(x: 0, y: 0, width: 8, height: 8)
+        ctx.setFillColor((UIColor(named: "SolidGrey50")?.cgColor)!)
+        ctx.fillEllipse(in: rect)
+        ctx.restoreGState()
+        let img = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        position.setThumbImage(img, for: .normal)
     }
-//
-//    func currentPositionForFirebase(){
-//        if(currentAdmin){let ref = Database.database().reference().child("/parties/\(currentParty)")
-//            ref.child("/currentlyPlaying").child("isPosition").setValue(0.0)}
-//        else{return}
-//    }
-
-    
-
-
-
-
-
-
+}
