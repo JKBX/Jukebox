a21f1d619991313db491e29b9f6b7180590594a4
Philipp Altmann <philippaltmann@deap.io>
2018-07-03 13:19:05 +0200

    Implemented Create Spotify Playlist feature

--- Jukebox/PartyOverview/PartyMenuViewController.swift
+++ Jukebox/PartyOverview/PartyMenuViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 import Firebase
-import Firebase
+import Alamofire
+import Spartan
 
 class PartyMenuViewController: UIViewController {
     
@@ -18,6 +19,7 @@ class PartyMenuViewController: UIViewController {
     var ref: DatabaseReference!
     var user = Auth.auth().currentUser?.uid
     var shareMessage: String!
+    @IBOutlet weak var playlistButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +51,32 @@ class PartyMenuViewController: UIViewController {
     
     func extinguishPartyFromFirebase() {
         self.ref.child("/users/\(user!)/parties/\(partyID!)").setValue(nil)
+    }
+    
+    @IBAction func writePlaylistToSpotify(_ sender: Any){
+        playlistButton.isEnabled = false
+        playlistButton.setTitle("Creating playlist ...", for: .disabled)
+        playlistButton.setTitleColor(.green, for: .disabled)
+        self.ref.child("/parties/\(partyID!)/history").observeSingleEvent(of: .value) { (snapshot) in
+            if snapshot.exists() {
+                if let session = SPTAuth.defaultInstance().session{
+                let history = snapshot.value as! NSDictionary
+                    var tracks: [String] = [""];
+                    for track in history.allKeys as! [String] {
+                        tracks.append("spotify:track:\(track)")
+                    }
+                    tracks.remove(at: 0)
+                print(history.allKeys)
+                    Spartan.authorizationToken = session.accessToken
+                    _ = Spartan.createPlaylist(userId: self.user!, name: self.partyName, isPublic: true, isCollaborative: false, success: { (playlist: Playlist) in
+                        _ = Spartan.addTracksToPlaylist(userId: self.user!, playlistId: playlist.id as! String, trackUris: tracks, success: { (snapshot) in
+                            self.playlistButton.isEnabled = true
+                            self.playlistButton.setTitle("Save Party as Spotify Playlist", for: .normal)
+                        }, failure: { (error) in print(error)})
+                    }, failure: { (error) in print(error)})
+                }
+            }
+        }
     }
     
     @IBAction func dismissPopUp(_ sender: ButtonDesignable) {
